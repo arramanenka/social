98ff000bddf492f6dfb80663a4b0e61743d7c9ab
aramanenka <arramanenka@gmail.com>
2020-07-09 16:47:11 +0300

    'initial'

---
+++ uservice/src/main/java/com/romanenko/uservice/UserviceApplication.java
@@ -0,0 +1,13 @@
+package com.romanenko.uservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserviceApplication.class, args);
+    }
+
+}
