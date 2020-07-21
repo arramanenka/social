e562bd269f52fc6e6e08c1267b24c2b8975e3893
aramanenka <arramanenka@gmail.com>
2020-07-21 16:17:14 +0300

    minor fixup for uniformity

--- uservice/src/main/java/com/romanenko/uservice/dao/impl/connection/ConnectionType.java
+++ uservice/src/main/java/com/romanenko/uservice/dao/impl/connection/ConnectionType.java
@@ -5,9 +5,13 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum ConnectionType {
-    FOLLOW("follow"),
-    BLACKLIST("blacklist"),
+    FOLLOW(ConnectionType.FOLLOW_NAME) {
+    },
+    BLACKLIST(ConnectionType.BLACKLIST_NAME),
+
     ;
+    public static final String FOLLOW_NAME = "follow";
+    public static final String BLACKLIST_NAME = "follow";
     @Getter
     private final String name;
 }
--- uservice/src/main/java/com/romanenko/uservice/dao/impl/user/neo/NeoUserRepo.java
+++ uservice/src/main/java/com/romanenko/uservice/dao/impl/user/neo/NeoUserRepo.java
@@ -1,5 +1,6 @@
 package com.romanenko.uservice.dao.impl.user.neo;
 
+import com.romanenko.uservice.dao.impl.connection.ConnectionType;
 import com.romanenko.uservice.model.User;
 import org.neo4j.springframework.data.repository.ReactiveNeo4jRepository;
 import org.neo4j.springframework.data.repository.query.Query;
@@ -12,12 +13,12 @@ public interface NeoUserRepo extends ReactiveNeo4jRepository<NeoUser, String> {
     Mono<Boolean> deleteByPuId(String puId);
 
     @Query("match (queryingPerson: " + PRIMARY_LABEL + ") where id(queryingPerson) = ?0 with queryingPerson" +
-            "match (person:" + PRIMARY_LABEL + ") where " + NAME_LABEL + " starts with ?1 AND (person)-[:blacklist*]-(queryingPerson)" +
+            "match (person:" + PRIMARY_LABEL + ") where " + NAME_LABEL + " starts with ?1 AND (person)-[:" + ConnectionType.BLACKLIST_NAME + "*]-(queryingPerson)" +
             "return person")
     Flux<NeoUser> getAllByNickBeginning(String id, String nickStart);
 
     @Query("match (queryingPerson: " + PRIMARY_LABEL + ") where id(queryingPerson) = ?0 with queryingPerson" +
-            "match (person:" + PRIMARY_LABEL + ") where id(person) = ?1 AND (person)-[:blacklist*]-(queryingPerson)" +
+            "match (person:" + PRIMARY_LABEL + ") where id(person) = ?1 AND (person)-[:" + ConnectionType.BLACKLIST_NAME + "*]-(queryingPerson)" +
             "return person")
     Mono<User> findUserById(String queryingId, String id);
 
