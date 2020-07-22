9017a9723a8556529fb690bd87659c083e99bee4
aramanenka <arramanenka@gmail.com>
2020-07-22 19:15:28 +0300

    add required fields

--- uservice/src/main/java/com/romanenko/model/User.java
+++ uservice/src/main/java/com/romanenko/model/User.java
@@ -1,5 +1,6 @@
 package com.romanenko.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,6 +8,8 @@ import lombok.Data;
 @Builder
 public class User {
     private String id;
+    @JsonProperty(required = true)
     private String name;
+    @JsonProperty(required = true)
     private String avatarUrl;
 }
