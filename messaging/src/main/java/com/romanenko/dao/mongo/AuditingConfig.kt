8ca59ba70f9e54265c454f7bb72c403c288ef706
aramanenka <arramanenka@gmail.com>
2020-08-08 14:53:34 +0300

    conditional on config

--- messaging/src/main/java/com/romanenko/dao/mongo/AuditingConfig.kt
+++ messaging/src/main/java/com/romanenko/dao/mongo/AuditingConfig.kt
@@ -1,8 +1,10 @@
 package com.romanenko.dao.mongo
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.EnableMongoAuditing
 
 @Configuration
 @EnableMongoAuditing
+@ConditionalOnProperty(name = ["message.storage"], havingValue = "mongo", matchIfMissing = true)
 open class AuditingConfig
