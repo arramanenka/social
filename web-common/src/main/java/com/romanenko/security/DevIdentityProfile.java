2776a18a94ac3f09a3cef68c0b1a7d20a7ebb84b
aramanenka <arramanenka@gmail.com>
2020-09-20 12:17:52 +0300

    cors config

--- web-common/src/main/java/com/romanenko/security/DevIdentityProfile.java
+++ web-common/src/main/java/com/romanenko/security/DevIdentityProfile.java
@@ -1,10 +1,17 @@
 package com.romanenko.security;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import reactor.core.publisher.Mono;
 
+@Log4j2
 @Profile("dev")
 @Component
 public class DevIdentityProfile {
@@ -12,6 +19,22 @@ public class DevIdentityProfile {
     @Bean
     public IdentityProvider identityProvider() {
         return request -> Mono.just(() -> request.queryParam("id").orElse("id"));
+    }
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        var configSource = new CorsConfiguration();
+        configSource.addAllowedOrigin("*");
+        for (HttpMethod value : HttpMethod.values()) {
+            configSource.addAllowedMethod(value);
+        }
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configSource);
+
+        http
+                .cors().configurationSource(source);
+        log.info("Disabled cors and csrf for dev mode");
+        return http.build();
     }
 
 }
